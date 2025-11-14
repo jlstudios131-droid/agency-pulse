2fe5212a6f0d26e3a04d711ab93d0380d61e323e
JLStudioOficiall <jlstudios131@gmail.com>
2025-11-15 00:36:01 +0100

    Create Header.tsx

---
+++ lib/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full py-4 px-6 bg-white border-b">
+      <h1 className="text-xl font-semibold">AgencyPulse</h1>
+    </header>
+  );
+}
