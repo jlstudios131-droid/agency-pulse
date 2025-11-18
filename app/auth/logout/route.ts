f858492af3a43a7827ca2713c758304f9ecb5846
JLStudioOficiall <jlstudios131@gmail.com>
2025-11-18 10:09:28 +0100

    Create route.ts

---
+++ app/auth/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { createSupabaseServerClient } from "@/lib/supabaseServer";
+
+export async function POST() {
+  const supabase = createSupabaseServerClient();
+
+  await supabase.auth.signOut();
+
+  return NextResponse.redirect("/auth/login", {
+    status: 302,
+  });
+}
