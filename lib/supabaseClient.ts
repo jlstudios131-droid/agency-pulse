30a409cb645342ea594c5221de95d0ee42512308
JLStudioOficiall <jlstudios131@gmail.com>
2025-11-18 09:09:54 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export function createSupabaseBrowserClient() {
+  return createClientComponentClient();
+}
